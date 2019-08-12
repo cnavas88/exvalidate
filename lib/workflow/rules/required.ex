77f44d3b2f0ef7816ac05855e4b9f6cd3b738f98
Carlos Navas <dante.maniacs@gmail.com>
2019-08-12 15:26:59 +0200

    testing the required rule.

--- lib/workflow/rules/required.ex
+++ lib/workflow/rules/required.ex
@@ -1,17 +1,21 @@
 defmodule Exvalidate.Rules.Required do
   @moduledoc false
 
+  # @spec validating(map, String.t, map) :: {:ok, map} || {:error, String.t}
+
   def validating(%{"required" => true}, field, data) do
     if conditions(field, data) do
       {:ok, data}
     else
-      {:error, "#{field} is required"}
+      {:error, "#{field} is required."}
     end
   end
-
   def validating(%{"required" => false}, field, data) do
     {:ok, data}
   end
+  def validating(_, _, _), do: {:error, "Rule required is wrong."}
+
+  @spec conditions(String.t, map) :: boolean
 
   defp conditions(field, data) do
     Map.has_key?(data, field) &&
