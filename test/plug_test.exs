61e2b31e0f503dfd3566702b08d9caa52975f597
Carlos Navas <dante.maniacs@gmail.com>
2019-10-17 14:07:25 +0200

    adding more testing for plug errors and custom error function

--- test/plug_test.exs
+++ test/plug_test.exs
@@ -47,53 +47,74 @@ defmodule Exvalidate.PlugTest do
     end
   end
 
+  defmodule TestRouterCustomErrorFn do
+    use Plug.Router
+
+    plug(Plug.Parsers, parsers: [:urlencoded, :multipart])
+    plug(:match)
+    plug(PlugValidate, on_error: &Exvalidate.PlugTest.custom_error/2)
+    plug(:dispatch)
+
+    @schema %{
+      "id" => %{"required" => true}
+    }
+
+    get "/test", private: %{validate_query: @schema} do
+      Plug.Conn.send_resp(conn, 200, "")
+    end
+
+    post "/test", private: %{validate_body: @schema} do
+      Plug.Conn.send_resp(conn, 200, "")
+    end
+  end
+
   describe "Testing plug with json errors" do
     test "QueryString validation with valid query params" do
       conn =
         :get
         |> Plug.Test.conn("/test?id=123")
         |> TestRouterJson.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 200
       assert conn.query_params == %{"id" => "123"}
       assert conn.resp_body == ""
     end
-  
+
     test "QueryString Validation error" do
       conn =
         :get
         |> Plug.Test.conn("/test?id=&name=Boo")
         |> TestRouterJson.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 400
       assert conn.query_params == %{"id" => "", "name" => "Boo"}
       assert Jason.decode!(conn.resp_body) == %{"error" => "id is required."}
     end
-  
+
     test "Body validation with valid body params" do
       conn =
         :post
         |> Plug.Test.conn("/test", %{"id" => "123"})
         |> TestRouterJson.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 200
       assert conn.body_params == %{"id" => "123"}
     end
-  
+
     test "Body validation error" do
       conn =
         :post
         |> Plug.Test.conn("/test", %{"id" => ""})
         |> TestRouterJson.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 400
       assert conn.body_params == %{"id" => ""}
       assert Jason.decode!(conn.resp_body) == %{"error" => "id is required."}
-    end  
+    end
   end
 
   describe "Testing plug with text plain errors" do
@@ -102,46 +123,81 @@ defmodule Exvalidate.PlugTest do
         :get
         |> Plug.Test.conn("/test?id=123")
         |> TestRouterPlainText.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 200
       assert conn.query_params == %{"id" => "123"}
       assert conn.resp_body == ""
     end
-  
+
     test "QueryString Validation error" do
       conn =
         :get
         |> Plug.Test.conn("/test?id=&name=Boo")
         |> TestRouterPlainText.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 400
       assert conn.query_params == %{"id" => "", "name" => "Boo"}
       assert conn.resp_body == "id is required."
     end
-  
+
     test "Body validation with valid body params" do
       conn =
         :post
         |> Plug.Test.conn("/test", %{"id" => "123"})
         |> TestRouterPlainText.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 200
       assert conn.body_params == %{"id" => "123"}
     end
-  
+
     test "Body validation error" do
       conn =
         :post
         |> Plug.Test.conn("/test", %{"id" => ""})
         |> TestRouterPlainText.call([])
-  
+
       assert conn.state == :sent
       assert conn.status == 400
       assert conn.body_params == %{"id" => ""}
       assert conn.resp_body == "id is required."
-    end  
+    end
+  end
+
+  describe "Testing plug with custom function error." do
+    test "QueryString Validation error" do
+      conn =
+        :get
+        |> Plug.Test.conn("/test?id=&name=Boo")
+        |> TestRouterCustomErrorFn.call([])
+
+      assert conn.state == :sent
+      assert conn.status == 400
+      assert conn.query_params == %{"id" => "", "name" => "Boo"}
+      assert conn.resp_body == "id is required. CUSTOM"
+    end
+
+    test "Body validation error" do
+      conn =
+        :post
+        |> Plug.Test.conn("/test", %{"id" => ""})
+        |> TestRouterCustomErrorFn.call([])
+
+      assert conn.state == :sent
+      assert conn.status == 400
+      assert conn.body_params == %{"id" => ""}
+      assert conn.resp_body == "id is required. CUSTOM"
+    end
+  end
+
+  # AUXILIAR FUNCTIONS
+
+  def custom_error(conn, error_message) do
+    conn
+    |> Plug.Conn.put_resp_header("content-type", "text/plain")
+    |> Plug.Conn.send_resp(400, error_message <> " CUSTOM")
+    |> Plug.Conn.halt()
   end
 end
