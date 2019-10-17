8420f1516afdee40a26a2e8869fa92dad3594d42
Carlos Navas <dante.maniacs@gmail.com>
2019-10-17 15:27:04 +0200

    adding exvalidate testing

--- test/exvalidate_test.exs
+++ test/exvalidate_test.exs
@@ -2,7 +2,97 @@ defmodule ExvalidateTest do
   use ExUnit.Case
   doctest Exvalidate
 
-  # test "greets the world" do
-  #   assert Exvalidate.hello() == :world
-  # end
+  alias Exvalidate
+
+  # TODO - HACER TEST DE INTEGRACION MEJORES PARA TODOS LOS CASOS POSIBLES
+
+  describe "Unit tests." do
+    test "Not validate allowed params." do
+      data = %{
+        "name" => "Vegeta"
+      }
+      
+      schema = %{
+        "id" => %{"required" => true}
+      }
+      result = Exvalidate.validate(data, schema)
+
+      assert result == {:error, "id is not allowed"}
+    end
+
+    test "Validate allowed params but not validate schema" do
+      data = %{
+        "id" => 12345,
+        "name" => ""
+      }
+
+      deps = %{
+        validate: fn _, _, _ -> {:error, "Name is required."} end
+      }
+      
+      schema = %{
+        "name" => %{"required" => true}
+      }
+      result = Exvalidate.validate(data, schema, deps)
+
+      assert result == {:error, "Name is required."}
+    end
+
+    test "Validate allowed params and validate schema" do
+      data = %{
+        "id" => 12345,
+        "name" => "Carlos"
+      }
+      
+      schema = %{
+        "name" => %{"required" => true}
+      }
+
+      result = Exvalidate.validate(data, schema)
+
+      assert result == {:ok, data}
+    end
+  end
+
+  describe "Functional and integration tests." do
+    test "Not validate allowed params" do
+      data = %{"name" => "Vegeta"}
+      
+      schema = %{"id" => %{"required" => true}}
+      result = Exvalidate.validate(data, schema)
+
+      assert result == {:error, "id is not allowed"}
+    end
+
+    test "Validate allowed params but not validate schema" do
+      data = %{
+        "id" => 12345,
+        "name" => ""
+      }
+      
+      schema = %{
+        "id" => %{"required" => false},
+        "name" => %{"required" => true}
+      }
+      result = Exvalidate.validate(data, schema)
+
+      assert result == {:error, "name is required."}
+    end
+
+    test "Validate allowed params and validate schema" do
+      data = %{
+        "id" => 12345,
+        "name" => "Carlos"
+      }
+      
+      schema = %{
+        "id" => %{"required" => true},
+        "name" => %{"max_length" => 8}
+      }
+
+      result = Exvalidate.validate(data, schema)
+
+      assert result == {:ok, data}
+    end
+  end
 end
